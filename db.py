7770fc01fd8e16ea9a36dff2f2a85702187cc617
alexalaver <alex.alaverdyan.04@gmail.com>
2023-07-05 01:41:12 +0400

    Haishin bot version 2.0

--- db.py
+++ db.py
@@ -18,23 +18,60 @@ class Data:
 
     def add_message(self, id, option, name, tel):
         with self.connect:
-            self.connect.execute("INSERT INTO orders(id, option, name, tel) VALUES(?, ?, ?, ?)", (id, option, name, tel,))
+            self.cursor.execute("INSERT INTO orders(id, option, name, tel) VALUES(?, ?, ?, ?)", (id, option, name, tel,))
             self.connect.commit()
 
     def add_option(self, id, option):
         with self.connect:
-            self.connect.execute("INSERT INTO cashe(id, option) VALUES(?, ?)", (id, option))
+            self.cursor.execute("INSERT INTO cashe(id, option) VALUES(?, ?)", (id, option))
 
     def add_name_cashe(self, id, us_text):
         with self.connect:
-            self.connect.execute("UPDATE cashe SET us_text = ? WHERE id = ?", (us_text, id,))
+            self.cursor.execute("UPDATE cashe SET us_text = ? WHERE id = ?", (us_text, id,))
             self.connect.commit()
 
     def set_cashe(self, id):
         with self.connect:
-            return self.connect.execute("SELECT us_text, option FROM cashe WHERE id = ?", (id,)).fetchone()
+            return self.cursor.execute("SELECT us_text, option FROM cashe WHERE id = ?", (id,)).fetchone()
 
     def delete_cashe(self, id):
         with self.connect:
-            self.connect.execute("DELETE FROM cashe WHERE id = ?", (id,))
+            self.cursor.execute("DELETE FROM cashe WHERE id = ?", (id,))
             self.connect.commit()
+
+    def delete_all_cashe(self):
+        with self.connect:
+            self.cursor.execute("DELETE FROM cashe")
+            self.connect.commit()
+
+    def delete_all_orders(self):
+        with self.connect:
+            self.cursor.execute("DELETE FROM orders")
+            self.connect.commit()
+
+    def check_adm(self, id):
+        with self.connect:
+            result = self.cursor.execute("SELECT lvl FROM users WHERE id=?", (id,)).fetchone()
+            if result[0] == 0:
+                return False
+            elif result[0] == 1:
+                return True
+
+    def get_adm(self, id):
+        with self.connect:
+            result = self.cursor.execute("SELECT lvl FROM users WHERE id=?", (id,)).fetchone()[0]
+            if result == 0:
+                self.cursor.execute("UPDATE users SET lvl = 1 WHERE id=?", (id,))
+                self.connect.commit()
+            else:
+                return False
+
+    def del_adm(self, id):
+        with self.connect:
+            result = self.cursor.execute("SELECT lvl FROM users WHERE id=?", (id,)).fetchone()[0]
+            if result == 1:
+                self.cursor.execute("UPDATE users SET lvl = 0 WHERE id=?", (id,))
+                self.connect.commit()
+            else:
+                return False
+
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from aiogram.dispatcher.storage import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import config as cfg
 import logging
-import time
 from db import Data
 
 db = Data("database.db")
@@ -23,7 +22,7 @@ async def start(message: types.Message):
     if message.chat.type == "private":
         if(not db.check_user(message.from_user.id)):
             db.add_user(message.from_user.id, message.from_user.first_name, message.from_user.username)
-
+        db.delete_cashe(message.from_user.id)
         markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
         button1 = types.KeyboardButton(cfg.but1)
         button2 = types.KeyboardButton(cfg.but2)
@@ -32,6 +31,24 @@ async def start(message: types.Message):
         markup.add(button1, button2, button3, button4)
         await message.answer(cfg.begin, reply_markup=markup)
 
+
+@dp.message_handler(commands=['delete_cashe'])
+async def delete_cashe(message: types.Message):
+    if message.chat.type == "private":
+        if db.check_adm(message.from_user.id) is True:
+            db.delete_all_cashe()
+            await message.answer(cfg.right_deal)
+        else:
+            await message.answer(cfg.false_deal)
+
+@dp.message_handler(commands=['delete_orders'])
+async def delete_cashe(message: types.Message):
+    if message.chat.type == "private":
+        if db.check_adm(message.from_user.id) is True:
+            db.delete_all_orders()
+            await message.answer(cfg.right_deal)
+        else:
+            await message.answer(cfg.false_deal)
 
 @dp.message_handler(state=info_reg.name)
 async def inforeg(message: types.Message, state: FSMContext):
@@ -42,12 +59,14 @@ async def inforeg(message: types.Message, state: FSMContext):
         button3 = types.KeyboardButton(cfg.but3)
         button4 = types.KeyboardButton(cfg.but4)
         markup.add(button1, button2, button3, button4)
-        if message.text == "but6":
-            await state.reset_state()
-            await message.answer(cfg.cancel, reply_markup=markup)
-        elif message.text == "but5":
-            await state.reset_state()
+        if message.text == cfg.but5:
             await message.answer(cfg.return1, reply_markup=markup)
+            db.delete_cashe(message.from_user.id)
+            await state.reset_state()
+        elif message.text == cfg.but6:
+            await message.answer(cfg.cancel, reply_markup=markup)
+            db.delete_cashe(message.from_user.id)
+            await state.reset_state()
         else:
             if type(message.text) == str:
                 if 2 <= len(message.text) <= 50:
@@ -73,12 +92,14 @@ async def inforeg1(message: types.Message, state: FSMContext):
             button3 = types.KeyboardButton(cfg.but3)
             button4 = types.KeyboardButton(cfg.but4)
             markup.add(button1, button2, button3, button4)
-            if message.text == "but6":
-                await state.reset_state()
-                await message.answer(cfg.cancel, reply_markup=markup)
-            elif message.text == "but5":
-                await state.reset_state()
+            if message.text == cfg.but5:
                 await message.answer(cfg.return1, reply_markup=markup)
+                db.delete_cashe(message.from_user.id)
+                await state.reset_state()
+            elif message.text == cfg.but6:
+                await message.answer(cfg.cancel, reply_markup=markup)
+                db.delete_cashe(message.from_user.id)
+                await state.reset_state()
             else:
                 if message.text[0] == "+" and type(int(message.text[1:])) == int or type(int(message.text)) == int:
                     if 5 <= len(message.text) <= 50:
@@ -111,13 +132,24 @@ async def default(message: types.Message):
                 markup.add(button1, button2)
                 await message.answer(cfg.send_your_contact, reply_markup=markup)
                 option = message.text
+                db.delete_cashe(message.from_user.id)
                 db.add_option(message.from_user.id, option)
-                time.sleep(1)
                 await message.answer(cfg.register1)
                 await info_reg.name.set()
             except Exception as e:
                 db.delete_cashe(message.from_user.id)
                 print(e)
+        elif message.text == cfg.but5:
+            markup1 = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+            button1 = types.KeyboardButton(cfg.but1)
+            button2 = types.KeyboardButton(cfg.but2)
+            button3 = types.KeyboardButton(cfg.but3)
+            button4 = types.KeyboardButton(cfg.but4)
+            markup1.add(button1, button2, button3, button4)
+            await message.answer(cfg.return2, reply_markup=markup1)
+        elif message.text == cfg.but6:
+            await message.answer(cfg.false_cancel)
+
 
 
 if __name__ == "__main__":
